dd25e41429f9e0dd8f10cc4e36d69d694d55b3f0
maestroanth <maestroanth@gmail.com>
2017-05-01 16:29:05 -0600

    Just added some more test stuff missing from MVA tutorial. Routing stuff has changed a lot but added the general jist

--- CourseTro/CourseApp/src/app/app.component.ts
+++ CourseTro/CourseApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, Directive, Injectable, NgModule, ViewContainerRef, TemplateRef, VERSION, OnInit } from '@angular/core';
+import { Routes, RouterModule, ROUTER_CONFIGURATION, ROUTER_INITIALIZER, ɵROUTER_PROVIDERS } from '@angular/router';
 import { DataService } from './data.service';
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   template: `
-
+  
 
 <!--<h1 [class.red-title]="titleClass">-->
 <!-- <h1 [style.color] = "titleStyle ? 'green' : 'pink'" >-->
@@ -28,8 +29,25 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
   <br /><br />
 
   <br />
+<p> 2-way Binding Test </p>
 
+   <input [(ngModel)]="name" #ctrl="ngModel" required>
+    <p>Value: {{ name }}</p>
+    <p>Valid: {{ ctrl.valid }}</p>
+    <button (click)="setValue()">Set value</button>
 
+    <p> Array as JSON object test </p>
+    <p>{{num | json}}</p>
+
+    <p> Routing Test</p>
+<!--
+    <nav>
+        <a href="" [routerLink]="['Notes']> Notes</a>
+        <a href="" [routerLink]="['Tasks']> Tasks</a>
+    </nav>
+
+<router-outlet></router-outlet>
+-->
 `,
   styles: [`
   h1 {
@@ -56,22 +74,26 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
   `],
 
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   //properties and methods are defined here'
   //also where dependency injection occurs within a contructor which gives access to various services/providers
 
-  
+    num: Array<number> = [7, 4, 2];
+
+    name: string = '';
+    setValue() { this.name = 'Nancy'; }
 
     constructor(private dataService: DataService) {
-
+        this.bitchArray = this.dataService.myBitches();//bitchesName going through these cycles is called data interporlation
     }
 
 
     bitchArray = new Array;
 
     ngOnInit() { //lifecycle hook
-        console.log(this.dataService.bitches);
-        this.bitchArray = this.dataService.myBitches();//bitchesName going through these cycles is called data interporlation
+        console.log("Init Hook: " + this.dataService.bitches);  
     }
 
 
@@ -93,5 +115,25 @@ export class AppComponent {
       'large-title': true
   }
   //buttonStatus = 'enabled';
+  //You're suppose to put routes in another file i.e. 'app.routes.ts'
 
 }
+
+const routes: Routes = [
+    {
+        path: 'index.html',
+        component: AppComponent,
+    },
+    {
+        path: 'index.html/:id',
+        component: AppComponent
+    },
+    {
+        path: '',
+        redirectTo: '/index.html',
+        pathMatch: 'full'
+    },
+];
+
+//updated export
+export const routing = RouterModule.forRoot(routes);
